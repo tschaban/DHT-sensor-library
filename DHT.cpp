ec0466384c8b09911cd59e75a8b2162871aebc4e
Tschaban-A <github@adrian.czabanowski.com>
2018-11-01 10:30:29 +0100

    Added additiona constructors

--- DHT.cpp
+++ DHT.cpp
@@ -8,17 +8,24 @@ written by Adafruit Industries
 
 #define MIN_INTERVAL 2000
 
+DHT::DHT() {}
+
 DHT::DHT(uint8_t pin, uint8_t type, uint8_t count) {
   _pin = pin;
   _type = type;
-  #ifdef __AVR
-    _bit = digitalPinToBitMask(pin);
-    _port = digitalPinToPort(pin);
-  #endif
-  _maxcycles = microsecondsToClockCycles(1000);  // 1 millisecond timeout for
-                                                 // reading pulses from DHT sensor.
+#ifdef __AVR
+  _bit = digitalPinToBitMask(pin);
+  _port = digitalPinToPort(pin);
+#endif
+  _maxcycles =
+      microsecondsToClockCycles(1000); // 1 millisecond timeout for
+                                       // reading pulses from DHT sensor.
   // Note that count is now ignored as the DHT reading algorithm adjusts itself
   // basd on the speed of the processor.
+}
+
+void DHT::init(uint8_t pin, uint8_t type, uint8_t count) {
+  DHT(pin, type, count);
 }
 
 void DHT::begin(void) {
@@ -28,10 +35,11 @@ void DHT::begin(void) {
   // >= MIN_INTERVAL right away. Note that this assignment wraps around,
   // but so will the subtraction.
   _lastreadtime = -MIN_INTERVAL;
-  DEBUG_PRINT("Max clock cycles: "); DEBUG_PRINTLN(_maxcycles, DEC);
+  DEBUG_PRINT("Max clock cycles: ");
+  DEBUG_PRINTLN(_maxcycles, DEC);
 }
 
-//boolean S == Scale.  True == Fahrenheit; False == Celcius
+// boolean S == Scale.  True == Fahrenheit; False == Celcius
 float DHT::readTemperature(bool S, bool force) {
   float f = NAN;
 
@@ -39,7 +47,7 @@ float DHT::readTemperature(bool S, bool force) {
     switch (_type) {
     case DHT11:
       f = data[2];
-      if(S) {
+      if (S) {
         f = convertCtoF(f);
       }
       break;
@@ -52,7 +60,7 @@ float DHT::readTemperature(bool S, bool force) {
       if (data[2] & 0x80) {
         f *= -1;
       }
-      if(S) {
+      if (S) {
         f = convertCtoF(f);
       }
       break;
@@ -61,13 +69,9 @@ float DHT::readTemperature(bool S, bool force) {
   return f;
 }
 
-float DHT::convertCtoF(float c) {
-  return c * 1.8 + 32;
-}
+float DHT::convertCtoF(float c) { return c * 1.8 + 32; }
 
-float DHT::convertFtoC(float f) {
-  return (f - 32) * 0.55555;
-}
+float DHT::convertFtoC(float f) { return (f - 32) * 0.55555; }
 
 float DHT::readHumidity(bool force) {
   float f = NAN;
@@ -88,8 +92,9 @@ float DHT::readHumidity(bool force) {
   return f;
 }
 
-//boolean isFahrenheit: True == Fahrenheit; False == Celcius
-float DHT::computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit) {
+// boolean isFahrenheit: True == Fahrenheit; False == Celcius
+float DHT::computeHeatIndex(float temperature, float percentHumidity,
+                            bool isFahrenheit) {
   // Using both Rothfusz and Steadman's equations
   // http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
   float hi;
@@ -97,23 +102,25 @@ float DHT::computeHeatIndex(float temperature, float percentHumidity, bool isFah
   if (!isFahrenheit)
     temperature = convertCtoF(temperature);
 
-  hi = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (percentHumidity * 0.094));
+  hi = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) +
+              (percentHumidity * 0.094));
 
   if (hi > 79) {
-    hi = -42.379 +
-             2.04901523 * temperature +
-            10.14333127 * percentHumidity +
-            -0.22475541 * temperature*percentHumidity +
-            -0.00683783 * pow(temperature, 2) +
-            -0.05481717 * pow(percentHumidity, 2) +
-             0.00122874 * pow(temperature, 2) * percentHumidity +
-             0.00085282 * temperature*pow(percentHumidity, 2) +
-            -0.00000199 * pow(temperature, 2) * pow(percentHumidity, 2);
+    hi = -42.379 + 2.04901523 * temperature + 10.14333127 * percentHumidity +
+         -0.22475541 * temperature * percentHumidity +
+         -0.00683783 * pow(temperature, 2) +
+         -0.05481717 * pow(percentHumidity, 2) +
+         0.00122874 * pow(temperature, 2) * percentHumidity +
+         0.00085282 * temperature * pow(percentHumidity, 2) +
+         -0.00000199 * pow(temperature, 2) * pow(percentHumidity, 2);
 
-    if((percentHumidity < 13) && (temperature >= 80.0) && (temperature <= 112.0))
-      hi -= ((13.0 - percentHumidity) * 0.25) * sqrt((17.0 - abs(temperature - 95.0)) * 0.05882);
+    if ((percentHumidity < 13) && (temperature >= 80.0) &&
+        (temperature <= 112.0))
+      hi -= ((13.0 - percentHumidity) * 0.25) *
+            sqrt((17.0 - abs(temperature - 95.0)) * 0.05882);
 
-    else if((percentHumidity > 85.0) && (temperature >= 80.0) && (temperature <= 87.0))
+    else if ((percentHumidity > 85.0) && (temperature >= 80.0) &&
+             (temperature <= 87.0))
       hi += ((percentHumidity - 85.0) * 0.1) * ((87.0 - temperature) * 0.2);
   }
 
@@ -147,8 +154,8 @@ boolean DHT::read(bool force) {
 
   uint32_t cycles[80];
   {
-    // Turn off interrupts temporarily because the next sections are timing critical
-    // and we don't want any interruptions.
+    // Turn off interrupts temporarily because the next sections are timing
+    // critical and we don't want any interruptions.
     InterruptLock lock;
 
     // End the start signal by setting data line high for 40 microseconds.
@@ -157,7 +164,7 @@ boolean DHT::read(bool force) {
 
     // Now start reading the data line to get the value from the DHT sensor.
     pinMode(_pin, INPUT_PULLUP);
-    delayMicroseconds(10);  // Delay a bit to let sensor pull data line low.
+    delayMicroseconds(10); // Delay a bit to let sensor pull data line low.
 
     // First expect a low signal for ~80 microseconds followed by a high signal
     // for ~80 microseconds again.
@@ -178,29 +185,29 @@ boolean DHT::read(bool force) {
     // then it's a 1.  We measure the cycle count of the initial 50us low pulse
     // and use that to compare to the cycle count of the high pulse to determine
     // if the bit is a 0 (high state cycle count < low state cycle count), or a
-    // 1 (high state cycle count > low state cycle count). Note that for speed all
-    // the pulses are read into a array and then examined in a later step.
-    for (int i=0; i<80; i+=2) {
-      cycles[i]   = expectPulse(LOW);
-      cycles[i+1] = expectPulse(HIGH);
+    // 1 (high state cycle count > low state cycle count). Note that for speed
+    // all the pulses are read into a array and then examined in a later step.
+    for (int i = 0; i < 80; i += 2) {
+      cycles[i] = expectPulse(LOW);
+      cycles[i + 1] = expectPulse(HIGH);
     }
   } // Timing critical code is now complete.
 
   // Inspect pulses and determine which ones are 0 (high state cycle count < low
   // state cycle count), or 1 (high state cycle count > low state cycle count).
-  for (int i=0; i<40; ++i) {
-    uint32_t lowCycles  = cycles[2*i];
-    uint32_t highCycles = cycles[2*i+1];
+  for (int i = 0; i < 40; ++i) {
+    uint32_t lowCycles = cycles[2 * i];
+    uint32_t highCycles = cycles[2 * i + 1];
     if ((lowCycles == 0) || (highCycles == 0)) {
       DEBUG_PRINTLN(F("Timeout waiting for pulse."));
       _lastresult = false;
       return _lastresult;
     }
-    data[i/8] <<= 1;
+    data[i / 8] <<= 1;
     // Now compare the low and high cycle times to see if the bit is a 0 or 1.
     if (highCycles > lowCycles) {
       // High cycles are greater than 50us low cycle count, must be a 1.
-      data[i/8] |= 1;
+      data[i / 8] |= 1;
     }
     // Else high cycles are less than (or equal to, a weird case) the 50us low
     // cycle count so this must be a zero.  Nothing needs to be changed in the
@@ -208,19 +215,23 @@ boolean DHT::read(bool force) {
   }
 
   DEBUG_PRINTLN(F("Received:"));
-  DEBUG_PRINT(data[0], HEX); DEBUG_PRINT(F(", "));
-  DEBUG_PRINT(data[1], HEX); DEBUG_PRINT(F(", "));
-  DEBUG_PRINT(data[2], HEX); DEBUG_PRINT(F(", "));
-  DEBUG_PRINT(data[3], HEX); DEBUG_PRINT(F(", "));
-  DEBUG_PRINT(data[4], HEX); DEBUG_PRINT(F(" =? "));
+  DEBUG_PRINT(data[0], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[1], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[2], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[3], HEX);
+  DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[4], HEX);
+  DEBUG_PRINT(F(" =? "));
   DEBUG_PRINTLN((data[0] + data[1] + data[2] + data[3]) & 0xFF, HEX);
 
   // Check we read 40 bits and that the checksum matches.
   if (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
     _lastresult = true;
     return _lastresult;
-  }
-  else {
+  } else {
     DEBUG_PRINTLN(F("Checksum failure!"));
     _lastresult = false;
     return _lastresult;
@@ -236,24 +247,24 @@ boolean DHT::read(bool force) {
 //   https://github.com/arduino/Arduino/blob/master/hardware/arduino/avr/cores/arduino/wiring_pulse.c
 uint32_t DHT::expectPulse(bool level) {
   uint32_t count = 0;
-  // On AVR platforms use direct GPIO port access as it's much faster and better
-  // for catching pulses that are 10's of microseconds in length:
-  #ifdef __AVR
-    uint8_t portState = level ? _bit : 0;
-    while ((*portInputRegister(_port) & _bit) == portState) {
-      if (count++ >= _maxcycles) {
-        return 0; // Exceeded timeout, fail.
-      }
+// On AVR platforms use direct GPIO port access as it's much faster and better
+// for catching pulses that are 10's of microseconds in length:
+#ifdef __AVR
+  uint8_t portState = level ? _bit : 0;
+  while ((*portInputRegister(_port) & _bit) == portState) {
+    if (count++ >= _maxcycles) {
+      return 0; // Exceeded timeout, fail.
     }
-  // Otherwise fall back to using digitalRead (this seems to be necessary on ESP8266
-  // right now, perhaps bugs in direct port access functions?).
-  #else
-    while (digitalRead(_pin) == level) {
-      if (count++ >= _maxcycles) {
-        return 0; // Exceeded timeout, fail.
-      }
+  }
+// Otherwise fall back to using digitalRead (this seems to be necessary on
+// ESP8266 right now, perhaps bugs in direct port access functions?).
+#else
+  while (digitalRead(_pin) == level) {
+    if (count++ >= _maxcycles) {
+      return 0; // Exceeded timeout, fail.
     }
-  #endif
+  }
+#endif
 
   return count;
 }
